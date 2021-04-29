9913f945ec45007ac300594f471a455a4d649d40
Judis <judismjeevan@gmail.com>
2021-04-29 21:47:12 +0530

    added fire emoji

--- assets/javascripts/script.js
+++ assets/javascripts/script.js
@@ -5,10 +5,10 @@ const options = {
     time: '0.5s', // default: '0.3s'
     mixColor: '#fff', // default: '#fff'
     backgroundColor: '#fff',  // default: '#fff'
-    buttonColorDark: '#100f2c',  // default: '#100f2c'
-    buttonColorLight: '#fff', // default: '#fff'
+    buttonColorDark: '#fff',  // default: '#100f2c'
+    buttonColorLight: '#000', // default: '#fff'
     saveInCookies: true, // default: true,
-    label: '🌓', // default: ''
+    label: '🔥', // default: ''
     autoMatchOsTheme: true // default: true
     }
 
